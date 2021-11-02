71fe70d97643edcf3f82ea552413c3b9d545f509
Kate Shauliuk <Katsiarynabaranava@gmail.com>
2021-11-02 19:41:59 +0300

    fixed buttons homework tests

--- src/test/java/herokuapp/AddDeleteElementsTests.java
+++ src/test/java/herokuapp/AddDeleteElementsTests.java
@@ -34,9 +34,9 @@ public class AddDeleteElementsTests {
         WebElement addButton = driver.findElement(By.xpath("//button[text()='Add Element']"));
         addButton.click();
         addButton.click();
-        List<WebElement> deleteButtons = driver.findElements(By.xpath("//button[text()='deleteElement']"));
+        List<WebElement> deleteButtons = driver.findElements(By.xpath("//button[text()='Delete']"));
         deleteButtons.get(0).click();
-        deleteButtons = driver.findElements(By.xpath("//button[text()='deleteElement']"));
+        deleteButtons = driver.findElements(By.xpath("//button[text()='Delete']"));
         Assert.assertEquals(deleteButtons.size(), 1);
     }
 }
